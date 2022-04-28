814a0ef7bde235f27d16d31ff07db1d64bcf7620
DrKelvin <kelvinronald12@gmail.com>
2022-04-28 19:17:01 -0300

    adicionando projetos

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function show_sobremim() {
   let div_sobremim = document.getElementById("sobremim");
+  let div_perfil = document.getElementById("perfil");
+
   div_sobremim.classList.remove("display_none");
   console.log("classe removida");
+
+  if(window.matchMedia("(max-width: 500px)").matches) {
+    div_perfil.classList.add("display_none");
+  } else {
+    
+  }
 }
 
 function rem_sobremim() {
